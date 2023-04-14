484e045b8e859d3208bae3c77993783ffa1f7338
freda <ndyfreda@gmail.com>
2023-04-14 10:29:25 -0400

    Array Filter lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+
+function findMatching(drivers, value) {
+    return drivers.filter(driver => driver.toLowerCase() === value.toLowerCase());
+  }
+
+function fuzzyMatch(drivers, value) {
+    return drivers.filter(driver => driver.startsWith(value));
+  }
+
+function matchName(drivers, value) {
+    return drivers.filter(driver => driver.name === value);
+  }
+      
